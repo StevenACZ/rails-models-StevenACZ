59250c6fa8c8ba019dc399fd72d7cbd956676592
stevenacz <scoailazaa@gmail.com>
2020-12-09 17:39:57 -0500

    Update the type of a column on the critics table

---
+++ db/migrate/20201209223422_change_body_from_string_to_text_on_critics.rb
@@ -0,0 +1,5 @@
+class ChangeBodyFromStringToTextOnCritics < ActiveRecord::Migration[6.0]
+  def change
+    change_column :critics, :body, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_223052) do
+ActiveRecord::Schema.define(version: 2020_12_09_223422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "critics", force: :cascade do |t|
-    t.string "body"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "game_id", null: false
