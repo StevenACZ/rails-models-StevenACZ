e2b5ff094af6b2c69241f35f1b161d491df72b8f
stevenacz <scoailazaa@gmail.com>
2020-12-09 18:02:59 -0500

    Add validations to the Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ApplicationRecord
+  validates :name, :genre, :price, :release_date, presence: true
+  validates :genre, inclusion: ['Simulator', 'Adventure', 'Strategy', 'Role-playing (RPG)', 'Shooter', 'Fighting', 'Sport']
+  validates :price, numericality: {greater_than_or_equal_to: 1}
 end
