c5b4f30480619800ac2d96c2c1001b331ac318b6
stevenacz <scoailazaa@gmail.com>
2020-12-09 18:54:13 -0500

    Add validations to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  validates :email, :username, presence: true, uniqueness: true
+  validates :username, length: { minimum: 6 }
+  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
+  validates :birth_date, :presence => true
+  validate :validate_age
+
+  private
+
+  def validate_age
+      if birth_date < 120.years.ago.to_date
+          errors.add(:birth_date, 'can\'t be more than 120 years ago and can\'t be in the future')
+      end
+  end
 end
